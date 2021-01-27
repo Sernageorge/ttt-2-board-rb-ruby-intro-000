7d7d5244ab5af7ebd0cd6fd57e8170dbf90a679d
George Serna <sernageorge@gmail.com>
2021-01-27 20:16:43 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,3 +1,4 @@
+def say_hello_world_ten_time
 phrase = "hello World!"
 puts phrase
 puts phrase
@@ -9,3 +10,8 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
+
+say_hello_world_ten_time
+
+say_hello_world_ten_time
